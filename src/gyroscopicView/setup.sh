70a58e7016d3f692441346047ed31ace6d852697
Nicolas Pilli <pillisan42@gmail.com>
2016-08-10 00:51:20 +0200

    renommage de setup en configure

--- src/gyroscopicView/setup.sh
+++
@@ -1,9 +0,0 @@
-#sudo apt-get install python-pip
-#sudo pip install -U catkin_tools
-bash
-git clone https://github.com/wjwwood/serial.git
-cd serial
-#if you want to install it in /usr/local you have to comment makefile CMAKE_INSTALL_PREFIX
-make install_deps
-make
-sudo make install
