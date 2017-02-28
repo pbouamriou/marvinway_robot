f0a4c840ab5708edfbfa2c126fbda26634a9373d
Guy <ge.dechabot@gmail.com>
2017-02-28 19:43:26 +0100

    debug du module inputoutput

--- src/igloo2/inputoutput/inputOutput.tcl
+++ src/igloo2/inputoutput/inputOutput.tcl
@@ -14,18 +14,15 @@ set UserTimeUnit ns
 restart -force -nowave 
  
 # add wave clk_i
-add wave cs_servo_i 
 add wave -radix hexadecimal addr_bus_i  		
 add wave -radix hexadecimal data_bus_io 
 
-add wave -radix hexadecimal servomoteur_status 
-add wave -radix hexadecimal servomoteur_frequence 
-add wave -radix hexadecimal servomoteur_angle 
-add wave -radix hexadecimal servomoteur_dutyCycle 
+add wave -radix hexadecimal input_output_io  
+add wave -radix hexadecimal input_output_value  
+add wave -radix hexadecimal input_output_direction  
+add wave -radix decimal offset_inout
 
-add wave write_i 
 
-add wave servomoteur_o
 
 proc ecritureAdresse {addresse  donnee} {
    force addr_bus_i [format {x"%X"} $addresse]	
@@ -59,33 +56,36 @@ force reset_i 1
 #Force Clk à 0 à l'instant présent t puis à 1 à l'instant t+10ns. Ce cycle est répété toutes les 20ns.
 force clk_i 0 0, 1 {10 ns} -r 20ns 
 
-force cs_servo_i 0	
+force cs_inout_i 0	
 force addr_bus_i  X"0"	
 force write_i 0	
 force read_i 0 
 run 500ns
 force reset_i 0 
-force cs_servo_i  1	
+force cs_inout_i  1	
 run 100ns
  
  
-ecritureAdresse 4 1
-ecritureAdresse 5 50000
-ecritureAdresse 6 180
+ecritureAdresse 1 65534
+ecritureAdresse 1 65535
+ecritureAdresse 1 65534
+ecritureAdresse 0 01
+force input_output_io(0) 0
+lectureAdresse 1
 
-run 50us
-run 50us
-ecritureAdresse 0 1
-ecritureAdresse 1 50000
-ecritureAdresse 2 90 
+force input_output_io(0) 1
 
-ecritureAdresse 4 0
+noforce input_output_io(0)
+ecritureAdresse 0 00
 
-run 50us
-run 50us
-ecritureAdresse 4 1
-ecritureAdresse 5 25000
-ecritureAdresse 6 0
 
-force cs_servo_i 0
-run 50us
+ecritureAdresse 1 65532
+ecritureAdresse 1 65535
+ecritureAdresse 1 65533
+ecritureAdresse 0 02
+force input_output_io(1) 1
+lectureAdresse 1
+
+
+
+run 10us
--- src/igloo2/inputoutput/inputoutput.vhd
+++ src/igloo2/inputoutput/inputoutput.vhd
@@ -19,7 +19,7 @@ entity inputOutput is
 		read_i  : in std_logic := 'X';
 		write_i : in std_logic := 'X';
 		
-		input_output_io : inout std_logic_vector(IN_OUT_N-1 downto 0)
+		input_output_io : inout std_logic_vector(15 downto 0)
 	);
 end inputOutput;
 
@@ -33,8 +33,10 @@ architecture behaviour of inputOutput is
 
 	type array_data_reg is array (IN_OUT_N-1 downto 0) of std_logic_vector(15 downto 0);
 		
-	signal input_output_direction : array_data_reg;
-	signal input_output_value : array_data_reg; 
+	-- signal input_output_direction : array_data_reg;
+	signal input_output_direction : std_logic_vector(15 downto 0);
+	-- signal input_output_value : array_data_reg;  
+	signal input_output_value : std_logic_vector(15 downto 0); 
 		 
 	signal not_reset: std_logic := '0';
 		
@@ -56,37 +58,51 @@ begin
 	begin 	
 		if reset_i = '1' then 
 			data_bus_io   <= (others => 'Z');		 
-			input_output_direction  <= (others => (others => '0')); 
-			input_output_value 		<= (others => (others => '1')); 
+			-- input_output_direction  <= (others => (others => '0')); 
+			input_output_direction  <= (others => '0'); 
+			-- input_output_value 		<= (others => (others => '0')); 
+			input_output_value 		<= (others => '0'); 
 		end if;
 		if rising_edge (clk_i)  then
 			if cs_inout_i = '1' then			
-				offset_inout <= to_integer(unsigned(addr_bus_i(14 downto 2)));
+				-- offset_inout <= to_integer(unsigned(addr_bus_i(14 downto 2)));
 				if fallingEdgeRead = '1'  then 
 					case addr_bus_i(1 downto 0) is
 						when "00" =>
-							data_bus_io(15 downto 0) <= input_output_direction(offset_inout);	
-						when "01" =>
-							data_bus_io(15 downto 0) <= input_output_value(offset_inout);	
+							-- data_bus_io(15 downto 0) <= input_output_direction(offset_inout);	
+							data_bus_io <= input_output_direction;	
+						when "01" =>	
+							-- data_bus_io(15 downto 0) <= input_output_value(offset_inout);	
+							data_bus_io <= input_output_value;	
 						when others =>	
 					end case;	
-				end if;
-				if risingEdgeWrite = '1' then
+				elsif risingEdgeWrite = '1' then
 					case addr_bus_i(1 downto 0) is
 						when "00" =>
-							 input_output_direction(offset_inout) <= data_bus_io(15 downto 0) ;	
+							input_output_direction <= data_bus_io ;	
 						when "01" =>
-							 input_output_value(offset_inout) <= data_bus_io(15 downto 0) ;	
-						when others =>		
-					end case;					
+							input_output_value <= data_bus_io ;								
+						when others =>			
+					end case;				
+				else
+					data_bus_io <= (others => 'Z');			
 				end if;
-			else
-				data_bus_io <= (others => 'Z');	
+				for offset_inputoutput in 0 to 15 loop
+					if input_output_direction(offset_inputoutput) = '1' then 
+						input_output_io(offset_inputoutput) <=  'Z';
+						input_output_value(offset_inputoutput) <= input_output_io(offset_inputoutput);
+					else
+						if input_output_value(offset_inputoutput) = '0' then  
+							input_output_io(offset_inputoutput) <=  '0';	
+						else
+							input_output_io(offset_inputoutput) <=  '1';
+						end if;
+					end if;
+				end loop;
 			end if;
 		end if;
 	end process decodage;
-	
-	
+			
 	fallingEdge_Read :edgeDetector 
 	generic map(
 		EDGE_DETECTOR => '0'
@@ -108,7 +124,5 @@ begin
 		signal_i =>  write_i,	
 		signal_o =>  risingEdgeWrite
 	);
-
-
 
 end behaviour;
